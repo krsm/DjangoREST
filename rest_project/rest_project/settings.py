de7ba4686e022aedf8da3db8b013eb7570523cd3
krsm <krsm184@gmail.com>
2017-03-25 14:45:30 -0400

    fixing typo

--- rest_project/rest_project/settings.py
+++ rest_project/rest_project/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfile
+    'django.contrib.staticfiles',
     # custom apps
     'rest_framework',
     'api',
