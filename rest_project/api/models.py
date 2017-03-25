383c5c1e1f82f0f906fe4b647ed4eefa1128a265
krsm <krsm184@gmail.com>
2017-03-25 14:56:19 -0400

    added serialize file, and updated the models

--- rest_project/api/models.py
+++ rest_project/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ToDoList(models.Model):
+
+    """ Class to represent the ToDoList Model """
+
+    name = models.CharField(max_length=150, blank=False)
+    description = models.TextField()
+    deadline = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+
+        """ Human readable representation of model instance """
+        return "{}".format(self.name)
+
--- rest_project/api/tests.py
+++ rest_project/api/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class ModelTestCase(TestCase):
+
+    """ Class to define the test suite fot the ToDoList model"""
+
+    def setUp(self):
+
+        pass
