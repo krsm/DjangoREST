442b52fde1e62e1fdb51ec715ab50dde5b58d6bb
krsm <krsm184@gmail.com>
2017-03-26 16:05:19 -0400

    added api/urls content

--- rest_project/api/views.py
+++ rest_project/api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import generics
+from .serializer import ToDoListSerializer
+from .models import ToDoList
+
+
+class CreateView(generics.ListCreateAPIView):
+
+    """ Class to define the behaviour REST Api"""
+
+    queryset = ToDoList.objects.all()
+    serializer_class = ToDoListSerializer
+
+    def perform_create(self, serializer):
+
+        """ Save the post data when creating a new todolist """
+
+        serializer.save()
+
